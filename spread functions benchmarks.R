a5818531335e94e867b04ec1b4c7bc671a56c141
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-03-02 19:49:52 -0300

    Update benchmarks

--- spread functions benchmarks.R
+++ spread functions benchmarks.R
@@ -3,7 +3,6 @@
 library(terra)
 library(tidyverse)
 library(Rcpp)
-library(viridis)
 library(microbenchmark)
 theme_set(theme_bw())
 
@@ -47,41 +46,10 @@ for(l in 1:ncol(land)) {
 }
 str(land_arr)
 
-# Benchmark fool vs cool versions, whole landscape burnable ----------------
-# cool version has vector and matrix form
-
-# fool version
-start_time_fool <- Sys.time()
-fire_fool <- simulate_fire_fool_cpp(
-  landscape = land[, 1:7],
-  burnable = rep(1, ncell(land_raster)),
-  ignition_cells = ig_location - 1,
-  n_rowcol = c(nrow(land_raster), ncol(land_raster)),
-  coef = coefs,
-  wind_column = 6 - 1,
-  elev_column = 7 - 1,
-  distances = distances,
-  upper_limit = 1
-)
-end_time_fool <- Sys.time()
-time_fool <- end_time_fool - start_time_fool
 
 
-# cool version vector
-start_time_cool <- Sys.time()
-fire_cool <- simulate_fire_cpp(
-  landscape = land[, 1:7],
-  burnable = rep(1, ncell(land_raster)),
-  ignition_cells = ig_location - 1,
-  n_rowcol = c(nrow(land_raster), ncol(land_raster)),
-  coef = coefs,
-  wind_column = 6 - 1,
-  elev_column = 7 - 1,
-  distances = distances,
-  upper_limit = 1
-)
-end_time_cool <- Sys.time()
-time_cool <- end_time_cool - start_time_cool
+# Benchmark
+
 
 # cool version matrix
 start_time_cool_m <- Sys.time()
@@ -98,23 +66,9 @@ fire_cool_m <- simulate_fire_mat_cpp(
 end_time_cool_m <- Sys.time()
 time_cool_m <- end_time_cool_m - start_time_cool_m
 
-# check all fires were large
-sum(fire_cool) / length(fire_cool)
-sum(fire_fool) / length(fire_fool)
-sum(as.numeric(fire_cool_m)) / length(fire_cool)
 
 # compare
-print(time_fool)
-print(time_cool)
 print(time_cool_m)
-
-# quotient:
-(as.numeric(time_cool / 60) / as.numeric(time_fool)) # fool is in min, cool is in sec.
-# 0.164394
-
-# cool matrix over cool vector
-(as.numeric(time_cool_m) / as.numeric(time_cool))
-# 0.2513233
 
 
 # __________________________________
@@ -123,28 +77,6 @@ print(time_cool_m)
 
 
 mbm_all <- microbenchmark(
-  fool = simulate_fire_fool_cpp(
-    landscape = land[, 1:7],
-    burnable = rep(1, ncell(land_raster)),
-    ignition_cells = ig_location - 1,
-    n_rowcol = c(nrow(land_raster), ncol(land_raster)),
-    coef = coefs,
-    wind_column = 6 - 1,
-    elev_column = 7 - 1,
-    distances = distances,
-    upper_limit = 1
-  ),
-  cool_vec = simulate_fire_cpp(
-    landscape = land[, 1:7],
-    burnable = rep(1, ncell(land_raster)),
-    ignition_cells = ig_location - 1,
-    n_rowcol = c(nrow(land_raster), ncol(land_raster)),
-    coef = coefs,
-    wind_column = 6 - 1,
-    elev_column = 7 - 1,
-    distances = distances,
-    upper_limit = 1
-  ),
   cool_mat = simulate_fire_mat_cpp(
     landscape = land_arr[, , 1:7],
     burnable = matrix(1, nrow(land_raster), ncol(land_raster)),
@@ -160,41 +92,6 @@ mbm_all <- microbenchmark(
 
 
 mbm_all
-
-# Benchmark fool vs cool versions, real burnable layer --------------------
-
-# fool version
-start_time_fool <- Sys.time()
-fire_fool <- simulate_fire_fool_cpp(
-  landscape = land[, 1:7],
-  burnable = land[, "burnable"],
-  ignition_cells = ig_location - 1,
-  n_rowcol = c(nrow(land_raster), ncol(land_raster)),
-  coef = coefs,
-  wind_column = 6 - 1,
-  elev_column = 7 - 1,
-  distances = distances,
-  upper_limit = 1
-)
-end_time_fool <- Sys.time()
-(time_fool <- end_time_fool - start_time_fool)
-
-
-# cool version
-start_time_cool <- Sys.time()
-fire_cool <- simulate_fire_cpp(
-  landscape = land[, 1:7],
-  burnable = land[, "burnable"],
-  ignition_cells = ig_location - 1,
-  n_rowcol = c(nrow(land_raster), ncol(land_raster)),
-  coef = coefs,
-  wind_column = 6 - 1,
-  elev_column = 7 - 1,
-  distances = distances,
-  upper_limit = 1
-)
-end_time_cool <- Sys.time()
-(time_cool <- end_time_cool - start_time_cool)
 
 # cool version matrix
 start_time_cool_m <- Sys.time()
@@ -212,24 +109,11 @@ end_time_cool_m <- Sys.time()
 time_cool_m <- end_time_cool_m - start_time_cool_m
 
 # check all fires were large
-sum(fire_cool) / sum(land[, "burnable"])
-sum(fire_fool) / sum(land[, "burnable"]) # OK
 sum(fire_cool_m %>% as.numeric) / sum(land[, "burnable"])
 # OK
 
 # compare
-print(time_fool)
-print(time_cool)
 print(time_cool_m)
-
-# quotient:
-(as.numeric(time_cool / 60) / as.numeric(time_fool)) # fool is in min, cool is in sec.
-# 0.05663786
-
-# mat over vector
-(as.numeric(time_cool_m) / as.numeric(time_cool)) # fool is in min, cool is in sec.
-
-
 
 # __________________________________
 
@@ -237,28 +121,6 @@ print(time_cool_m)
 
 
 mbm_real <- microbenchmark(
-  fool = simulate_fire_fool_cpp(
-    landscape = land[, 1:7],
-    burnable = land[, "burnable"],
-    ignition_cells = ig_location - 1,
-    n_rowcol = c(nrow(land_raster), ncol(land_raster)),
-    coef = coefs,
-    wind_column = 6 - 1,
-    elev_column = 7 - 1,
-    distances = distances,
-    upper_limit = 1
-  ),
-  cool_vec = simulate_fire_cpp(
-    landscape = land[, 1:7],
-    burnable = land[, "burnable"],
-    ignition_cells = ig_location - 1,
-    n_rowcol = c(nrow(land_raster), ncol(land_raster)),
-    coef = coefs,
-    wind_column = 6 - 1,
-    elev_column = 7 - 1,
-    distances = distances,
-    upper_limit = 1
-  ),
   cool_mat = simulate_fire_mat_cpp(
     landscape = land_arr[, , 1:7],
     burnable = land_arr[, , "burnable"],
