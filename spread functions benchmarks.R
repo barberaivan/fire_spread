a631c7265388c74cccebcf1e3b99d79eca9606bd
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-03-03 09:25:51 -0300

    Clean benchmarks

--- spread functions benchmarks.R
+++ spread functions benchmarks.R
@@ -15,7 +15,7 @@ land <- readRDS("data_cholila_landscape.rds")
 
 # cholila raster file
 land_raster <- rast("data_cholila_elevation.tif")
-nrow(land) == ncell(land_raster)
+assertthat::assert_that(nrow(land) == ncell(land_raster))
 
 # distances for 30 m resolution
 distances <- rep(res(land_raster)[1], 8) # sides
@@ -48,34 +48,7 @@ str(land_arr)
 
 
 
-# Benchmark
-
-
-# cool version matrix
-start_time_cool_m <- Sys.time()
-fire_cool_m <- simulate_fire_mat_cpp(
-  landscape = land_arr[, , 1:7],
-  burnable = matrix(1, nrow(land_raster), ncol(land_raster)),
-  ignition_cells = ig_location_mat - 1,
-  coef = coefs,
-  wind_layer = 6 - 1,
-  elev_layer = 7 - 1,
-  distances = distances,
-  upper_limit = 1
-)
-end_time_cool_m <- Sys.time()
-time_cool_m <- end_time_cool_m - start_time_cool_m
-
-
-# compare
-print(time_cool_m)
-
-
-# __________________________________
-
-# with microbenchmark
-
-
+# All terrain is burnable
 mbm_all <- microbenchmark(
   cool_mat = simulate_fire_mat_cpp(
     landscape = land_arr[, , 1:7],
@@ -90,36 +63,11 @@ mbm_all <- microbenchmark(
   times = 1
 )
 
-
+"With all terrain burnable"
 mbm_all
 
-# cool version matrix
-start_time_cool_m <- Sys.time()
-fire_cool_m <- simulate_fire_mat_cpp(
-  landscape = land_arr[, , 1:7],
-  burnable = land_arr[, , "burnable"],
-  ignition_cells = ig_location_mat - 1,
-  coef = coefs,
-  wind_layer = 6 - 1,
-  elev_layer = 7 - 1,
-  distances = distances,
-  upper_limit = 1
-)
-end_time_cool_m <- Sys.time()
-time_cool_m <- end_time_cool_m - start_time_cool_m
 
-# check all fires were large
-sum(fire_cool_m %>% as.numeric) / sum(land[, "burnable"])
-# OK
-
-# compare
-print(time_cool_m)
-
-# __________________________________
-
-# with microbenchmark
-
-
+# Only real burnable terrain is burnable
 mbm_real <- microbenchmark(
   cool_mat = simulate_fire_mat_cpp(
     landscape = land_arr[, , 1:7],
@@ -134,7 +82,8 @@ mbm_real <- microbenchmark(
   times = 1
 )
 
-mbm_all; mbm_real
+"With only real burnable terrain"
+mbm_real
 
 # Unit: seconds
 
